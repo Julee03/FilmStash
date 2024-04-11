47c1fe031cd520b26404df656c83ff7fc8e6648a
Julee03 <juleesingh0311@gmail.com>
2024-04-11 17:12:50 +0530

    removed browser in package.json

--- config-overrides.js
+++ config-overrides.js
@@ -3,12 +3,5 @@ module.exports = function override(config, env) {
     config.resolve.fallback = {
         crypto: require.resolve('crypto-browserify'),
     };
-    config.plugins.push(
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-            Buffer: ['buffer', 'Buffer'],
-        }),
-    );
-
     return config;
 }
